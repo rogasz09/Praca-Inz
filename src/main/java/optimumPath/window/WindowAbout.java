57057f590ba61f53041ac680b79d1933f4ae3385
rogasz09 <pawelrogasz@gmail.com>
2017-11-27 04:38:28 +0100

    Nowe okna About Settings

---
+++ src/main/java/optimumPath/window/WindowAbout.java
@@ -0,0 +1,97 @@
+package optimumPath.window;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+public class WindowAbout extends JDialog {
+
+	private static final long serialVersionUID = 661201541798286059L;
+	private final JPanel contentPanel = new JPanel();
+	private JButton cancelButton;
+
+	/**
+	 * Create the dialog.
+	 */
+	public WindowAbout() {
+		setTitle("Autorzy");
+		setResizable(false);
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		
+		JLabel lblName = new JLabel("Optymalna \u015Bcie\u017Cka na mapie rastrowej 3D");
+		lblName.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 18));
+		
+		JLabel lblAuthors = new JLabel("Autorzy: Pawe\u0142 Rogaszewski, Tomasz Paw\u0142owski");
+		lblAuthors.setFont(new Font("Segoe UI", Font.PLAIN, 15));
+		
+		JLabel lblPromoter = new JLabel("Promotor: dr in\u017C. Krystyna Rudzi\u0144ska-Korma\u0144ska");
+		lblPromoter.setFont(new Font("Segoe UI", Font.PLAIN, 15));
+		
+		JLabel lblCathedral = new JLabel("<html><center>Katedra System\u00F3w Decyzyjnych i Robotyki<br>\r\nETI Politechnika Gda\u0144ska</center>");
+		lblCathedral.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		GroupLayout gl_contentPanel = new GroupLayout(contentPanel);
+		gl_contentPanel.setHorizontalGroup(
+			gl_contentPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_contentPanel.createSequentialGroup()
+					.addContainerGap(47, Short.MAX_VALUE)
+					.addComponent(lblName, GroupLayout.PREFERRED_SIZE, 360, GroupLayout.PREFERRED_SIZE)
+					.addGap(29))
+				.addGroup(gl_contentPanel.createSequentialGroup()
+					.addGap(59)
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblAuthors)
+						.addGroup(gl_contentPanel.createParallelGroup(Alignment.TRAILING)
+							.addComponent(lblCathedral, GroupLayout.PREFERRED_SIZE, 302, GroupLayout.PREFERRED_SIZE)
+							.addComponent(lblPromoter)))
+					.addContainerGap(59, Short.MAX_VALUE))
+		);
+		gl_contentPanel.setVerticalGroup(
+			gl_contentPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPanel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblName, GroupLayout.PREFERRED_SIZE, 53, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblAuthors)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(lblPromoter)
+					.addGap(18)
+					.addComponent(lblCathedral)
+					.addContainerGap(36, Short.MAX_VALUE))
+		);
+		contentPanel.setLayout(gl_contentPanel);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				cancelButton = new JButton("Zamknij");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+		createEvents();
+	}
+	
+	private void createEvents() {
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+		});
+	}
+}
---
+++ src/main/java/optimumPath/window/WindowMapSettings.java
@@ -0,0 +1,197 @@
+package optimumPath.window;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+import optimumPath.common.Point3d;
+
+
+public class WindowMapSettings extends JDialog {
+
+	private static final long serialVersionUID = 6397790310030842569L;
+	private final JPanel contentPanel = new JPanel();
+	private JSpinner spnSizeX, spnSizeY, spnSizeZ;
+	private JSpinner spnRaster;
+	private JButton okButton;
+	private JButton cancelButton;
+	
+	private boolean isOk = false;
+
+
+	/**
+	 * Create the dialog.
+	 */
+	public WindowMapSettings() {
+		setTitle("Ustawienia Mapy");
+		setBounds(100, 100, 298, 301);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		
+		JLabel lblParametryMapa = new JLabel("Parametry mapa:");
+		lblParametryMapa.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		
+		JLabel lblRozmiarX = new JLabel("Rozmiar X:");
+		JLabel lblRozmiarY = new JLabel("Rozmiar Y:");
+		JLabel lblRozmiarZ = new JLabel("Rozmiar Z:");
+		JLabel lblRasterSize = new JLabel("Rozmiar rastra:");
+		
+		spnSizeX = new JSpinner();
+		spnSizeX.setModel(new SpinnerNumberModel(10, 3, 50, 1));
+		
+		spnSizeY = new JSpinner();
+		spnSizeY.setModel(new SpinnerNumberModel(10, 3, 50, 1));
+		
+		spnSizeZ = new JSpinner();
+		spnSizeZ.setModel(new SpinnerNumberModel(10, 1, 50, 1));
+		
+		spnRaster = new JSpinner();
+		spnRaster.setModel(new SpinnerNumberModel(1.0, 0.5, 3.0, 0.1));
+		
+		GroupLayout gl_contentPanel = new GroupLayout(contentPanel);
+		gl_contentPanel.setHorizontalGroup(
+			gl_contentPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPanel.createSequentialGroup()
+					.addGap(22)
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(lblParametryMapa)
+						.addGroup(gl_contentPanel.createSequentialGroup()
+							.addComponent(lblRasterSize)
+							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(spnRaster, GroupLayout.PREFERRED_SIZE, 83, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPanel.createSequentialGroup()
+							.addGroup(gl_contentPanel.createParallelGroup(Alignment.TRAILING)
+								.addComponent(lblRozmiarY, GroupLayout.PREFERRED_SIZE, 63, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblRozmiarX, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblRozmiarZ, GroupLayout.PREFERRED_SIZE, 63, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+								.addComponent(spnSizeZ, GroupLayout.PREFERRED_SIZE, 146, GroupLayout.PREFERRED_SIZE)
+								.addComponent(spnSizeY, GroupLayout.PREFERRED_SIZE, 146, GroupLayout.PREFERRED_SIZE)
+								.addComponent(spnSizeX, GroupLayout.PREFERRED_SIZE, 146, GroupLayout.PREFERRED_SIZE))))
+					.addContainerGap(42, Short.MAX_VALUE))
+		);
+		gl_contentPanel.setVerticalGroup(
+			gl_contentPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPanel.createSequentialGroup()
+					.addGap(19)
+					.addComponent(lblParametryMapa)
+					.addGap(18)
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblRasterSize)
+						.addComponent(spnRaster, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblRozmiarX)
+						.addComponent(spnSizeX, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblRozmiarY)
+						.addComponent(spnSizeY, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblRozmiarZ)
+						.addComponent(spnSizeZ, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(80, Short.MAX_VALUE))
+		);
+		contentPanel.setLayout(gl_contentPanel);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+		createEvents();
+	}
+	
+	private void createEvents() {
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				isOk = false;
+				dispose();
+			}
+		});
+		
+		okButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				isOk = true;
+				dispose();
+			}
+		});
+	}
+	
+	public double getSizeRaster() {
+		Double spnSizeRaster = (Double)spnRaster.getValue();
+		return spnSizeRaster.doubleValue();
+	}
+	
+	public Point3d getSizeMap() {
+		Integer spnSizeXValue = (Integer)spnSizeX.getValue();
+		Integer spnSizeYValue = (Integer)spnSizeY.getValue();
+		Integer spnSizeZValue = (Integer)spnSizeZ.getValue();
+		
+		Point3d size = new Point3d(spnSizeXValue.doubleValue(), spnSizeYValue.doubleValue(), spnSizeZValue.doubleValue());
+		return size;
+	}
+
+	public boolean isOk() {
+		return isOk;
+	}
+
+	public JSpinner getSpnSizeX() {
+		return spnSizeX;
+	}
+
+	public void setSpnSizeX(JSpinner spnSizeX) {
+		this.spnSizeX = spnSizeX;
+	}
+
+	public JSpinner getSpnSizeY() {
+		return spnSizeY;
+	}
+
+	public void setSpnSizeY(JSpinner spnSizeY) {
+		this.spnSizeY = spnSizeY;
+	}
+
+	public JSpinner getSpnSizeZ() {
+		return spnSizeZ;
+	}
+
+	public void setSpnSizeZ(JSpinner spnSizeZ) {
+		this.spnSizeZ = spnSizeZ;
+	}
+
+	public JSpinner getSpnRaster() {
+		return spnRaster;
+	}
+
+	public void setSpnRaster(JSpinner spnRaster) {
+		this.spnRaster = spnRaster;
+	}
+
+}
